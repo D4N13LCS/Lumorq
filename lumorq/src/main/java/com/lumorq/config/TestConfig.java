8eae5f9972f0d34d0c43c8489d3802ff232d8df8
D4N13LCS <danielmazzini6@gmail.com>
2026-05-27 01:47:24 -0300

    chore: configure H2 database and test profile

---
+++ lumorq/src/main/java/com/lumorq/config/TestConfig.java
@@ -0,0 +1,40 @@
+package com.lumorq.config;
+
+import java.time.Instant;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.lumorq.entities.Enrollment;
+import com.lumorq.entities.Student;
+import com.lumorq.entities.enums.EnrollmentStatus;
+import com.lumorq.repositories.EnrollmentRepository;
+import com.lumorq.repositories.StudentRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner{
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
+    private EnrollmentRepository enrollmentRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Student s1 = new Student(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        Student s2 = new Student(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
+
+        Enrollment e1 = new Enrollment(null, Instant.parse("2019-06-20T19:53:07Z"), EnrollmentStatus.PAID,s1);
+        Enrollment e2 = new Enrollment(null, Instant.parse("2019-07-21T03:42:10Z"), EnrollmentStatus.WAITING_PAYMENT,s2);
+        Enrollment e3 = new Enrollment(null, Instant.parse("2019-07-22T15:21:22Z"), EnrollmentStatus.WAITING_PAYMENT,s1);
+
+        studentRepository.saveAll(Arrays.asList(s1, s2));
+        enrollmentRepository.saveAll(Arrays.asList(e1,e2, e3));
+
+    }
+    
+}
