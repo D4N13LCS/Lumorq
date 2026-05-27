7e8c072c392ba1b96f4aff90271fa2a3b438605e
D4N13LCS <danielmazzini6@gmail.com>
2026-05-27 01:31:31 -0300

    chore: initialize Spring Boot project structure

---
+++ lumorq/src/main/java/com/lumorq/LumorqApplication.java
@@ -0,0 +1,13 @@
+package com.lumorq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumorqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumorqApplication.class, args);
+	}
+
+}
