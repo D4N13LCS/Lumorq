1eb389a12ad995bfe33d5713dc6d32f78e8030bf
D4N13LCS <danielmazzini6@gmail.com>
2026-05-27 01:36:50 -0300

    feat: implement Enrollment entity relationship

---
+++ lumorq/src/main/java/com/lumorq/entities/Enrollment.java
@@ -0,0 +1,100 @@
+package com.lumorq.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.lumorq.entities.enums.EnrollmentStatus;
+
+@Entity
+public class Enrollment implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private Instant moment;
+    
+    private Integer enrollmentStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    public Enrollment(){}
+
+    public Enrollment(Long id, Instant moment, EnrollmentStatus enrollmentStatus, Student student) {
+        this.id = id;
+        this.moment = moment;
+        setEnrollmentStatus(enrollmentStatus);
+        this.student = student;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public EnrollmentStatus getEnrollmentStatus() {
+        return EnrollmentStatus.valueOf(enrollmentStatus);
+    }
+
+    public void setEnrollmentStatus(EnrollmentStatus enrollmentStatus) {
+        if(enrollmentStatus != null){
+            this.enrollmentStatus = enrollmentStatus.getCode();
+        }
+        
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Enrollment other = (Enrollment) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+}
